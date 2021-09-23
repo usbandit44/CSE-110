37cfff47acb494b8562c55312b959fd954bb4e7a
Arvin <usbandit44@gmail.com>
2021-09-23 14:53:20 -0700

    finished deposit money into checkings

--- Assignment3.java
+++ Assignment3.java
@@ -3,11 +3,11 @@ import java.text.NumberFormat; // imports formater class
 
 public class Assignment3 {
     public static void main(String[] args){
-       double initialChecking;
-       double initialSaving;
+       double checking;
+       double saving;
        String fName;
        String lName;
-       Scanner in = new Scanner(System.in);
+       Scanner in = new Scanner(System.in); // initializing variables
 
        System.out.println("Welcome to CSE 110 bank");
        System.out.print("What is your first name? ");
@@ -15,23 +15,45 @@ public class Assignment3 {
        System.out.print("What is your last name? ");
        lName = in.next();
        System.out.print("Initial deposit into savings: ");
-       initialSaving = in.nextDouble();
+       saving = in.nextDouble();
        System.out.print("Initial deposit into checkings: ");
-       initialChecking = in.nextDouble();
+       checking = in.nextDouble(); // gets all the basic information for the account
 
-       double initialTotal = initialChecking + initialSaving;
+       double total = checking + saving;
+       double platiumInterest = .045;
        String userName = fName + lName;
        NumberFormat fmt = NumberFormat.getCurrencyInstance();
-       int i = 0;
+       int bankOption = 0; // the counter for the first set of choices
+       int choice = 0;
+       double depositAmount = 0;
 
-       if(initialTotal > 5000){
-           System.out.println(userName + "'s Platinum Account balance: " + fmt.format(initialTotal) + "(S: $" + fmt.format(initialSaving) + ", C: $" + fmt.format(initialChecking) + ")");
-           System.out.println("Bank Options:\n1. Deposit Money\n2. Withdraw Money\n3. Transfer money\n4. Quit");
+       if(total > 5000){
+           System.out.println(userName + "'s Platinum Account balance: " + fmt.format(total) + "(S: " + fmt.format(saving) + ", C: " + fmt.format(checking) + ")");
+           System.out.println("Bank Options:\n1. Deposit Money\n2. Withdraw Money\n3. Transfer Money\n4. Quit"); // creates the account and provides the user witht eh options
            System.out.print("Your Choice: ");
-           i = in.nextInt();
-           if(i == 1){
-               
-           }
+           bankOption = in.nextInt(); // has the user select their choice
+           
+            switch(bankOption){
+                case 1:
+                System.out.println("Deposit Money Options:\n1. Deposit to Checking\n2. Deposit to Savings\n3. Cancel");
+                System.out.print("Your Choice: ");
+                choice = in.nextInt();
+                switch(choice){
+                    case 1:
+                    System.out.print("Amount to deposit to Checking: ");
+                    depositAmount = in.nextDouble();
+                    checking = checking + depositAmount;
+                    total = checking + saving;
+                    System.out.println("You deposited " + fmt.format(depositAmount) + " into Checkings");
+                    System.out.println("After this transaction your balance is: " + fmt.format(total) + "(S: " + fmt.format(saving) + ", C:" + fmt.format(checking) + ")");
+                    double savingInterest = saving * platiumInterest + saving;
+                    double checkingInterest = checking * platiumInterest + checking;
+                    System.out.println("We have added interest to your account! (S: " + fmt.format(savingInterest) + ", C: " + fmt.format(checkingInterest) + ")");
+
+
+                }
+
+            }
 
        }
 
